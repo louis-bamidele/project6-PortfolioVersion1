e6c27926c30ae4f5579c0deddde61a775b095c32
louis <louisbamidele@icloud.com>
2023-08-28 00:45:32 +0100

    first comit

---
+++ README.md
@@ -0,0 +1,88 @@
+# E-commerce - headphone ecommerce website
+
+
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The features](#the-features)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+
+
+
+## Overview
+
+### The features
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- User registration and login with email, social media, or Google accounts.
+- Browse and search products by category, keywords, filters, etc.
+- Add, remove, or update items in the cart.
+- Calculate and display subtotal, taxes, and shipping costs.
+- Guest checkout or user account-based checkout.
+- Securely process payments through various methods (credit cards, PayPal, etc.).
+
+### Screenshot
+
+![](./screenshot.png)
+
+### Links
+
+- Solution URL: [solution](https://github.com/louis-bamidele/project5-SweetSound)
+- Live Site URL: [live site](https://silver-lebkuchen-79cc2f.netlify.app/)
+
+## My process
+
+### Built with
+
+- React
+- passsport
+- mongoDB
+- Stripe
+- Motion Framer
+- useQuary and alot more
+
+### What I learned
+
+I didn't know how to use Stripe, with this project I learned how to do that.
+
+See code snippets, see below:
+
+```Stripe
+ app.post("/create-checkout-session", async (req, res) => {
+  let listOfData = [];
+  let email = req.body.details.user.email;
+  req.body.cart.map((item) => {
+    let unitAmount = convertToUnitAmount(item.newPrice);
+    listOfData.push({
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: item.name,
+          images: item.images,
+        },
+        unit_amount: unitAmount,
+      },
+      quantity: item.numOfOrder,
+      adjustable_quantity: {
+        enabled: true,
+      },
+    });
+  });
+
+  const session = await stripe.checkout.sessions.create({
+    line_items: listOfData,
+    mode: "payment",
+    success_url: `${YOUR_DOMAIN}/successful`,
+    cancel_url: `${YOUR_DOMAIN}/checkout`,
+    customer_email: email,
+  });
+  res.send(session);
+})
+```
---
+++ main.js
@@ -0,0 +1,121 @@
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+const nameInput = document.getElementById("name");
+const Form = document.getElementById("form");
+const nameLabel = document.querySelector(".nameLabel");
+const emailInput = document.getElementById("email");
+const emailLabel = document.querySelector(".emailLabel");
+const textAreaInput = document.getElementById("textarea");
+const textLabel = document.querySelector(".textLabel");
+const btn = document.querySelector(".btn");
+const btn2 = document.querySelector(".btn2");
+const navBar = document.querySelector(".nav-bar");
+const buttons = document.querySelectorAll(".menu li");
+
+const buttonClicks = () => {
+  hamburger.dispatchEvent(new Event("click"));
+};
+for (let index of buttons) {
+  index.addEventListener("click", buttonClicks);
+}
+
+//  now adding event listener
+Form.addEventListener("submit", handleSubmit, false);
+hamburger.addEventListener("click", function () {
+  this.classList.toggle("is-active");
+  menu.classList.toggle("block");
+});
+nameInput.addEventListener("click", function () {
+  nameLabel.classList.add("on-click");
+});
+emailInput.addEventListener("click", function () {
+  emailLabel.classList.add("on-click");
+});
+textAreaInput.addEventListener("click", function () {
+  textLabel.classList.add("on-click");
+});
+btn.addEventListener("click", function () {
+  this.classList.toggle("click");
+  setTimeout(() => {
+    this.classList.toggle("click");
+  }, 200);
+});
+btn2.addEventListener("click", function () {
+  this.classList.toggle("click");
+  setTimeout(() => {
+    this.classList.toggle("click");
+  }, 100);
+});
+
+let oldValue = 0;
+let newValue = 0;
+window.addEventListener("scroll", (e) => {
+  newValue = window.pageYOffset;
+  if (oldValue < newValue) {
+    navBar.setAttribute("id", "navbar");
+  } else if (oldValue > newValue) {
+    navBar.removeAttribute("id", "navbar");
+  }
+  oldValue = newValue;
+});
+function handleInputChange(e) {
+  let { value, name } = e;
+  console.log(value, name);
+  let regex = "";
+  switch (name) {
+    case "name":
+      regex = /^[A-Za-z ]+$/;
+      break;
+    case "email":
+      regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+      break;
+    case "textarea":
+      regex = /^(\S+\s+)+\S+$/;
+      break;
+    default:
+      break;
+  }
+  let boolean = regex.test(value);
+  console.log(boolean);
+  console.log(value, name);
+  if (boolean) {
+    e.classList.remove("invalid-input");
+  } else {
+    e.classList.add("invalid-input");
+  }
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+
+  // index.js
+
+  const apiUrl = "https://sweetsound.onrender.com/contact-me";
+  const postData = {
+    message: nameInput.value,
+    email: emailInput.value,
+    name: textAreaInput.value,
+  };
+
+  const fetchOptions = {
+    method: "POST", // HTTP method
+    headers: {
+      "Content-Type": "application/json", // Specify the content type
+    },
+    body: JSON.stringify(postData), // Convert data to JSON format
+  };
+  console.log(postData);
+  fetch(apiUrl, fetchOptions)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Network response was not ok: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("POST response:", data);
+    })
+    .catch((error) => {
+      console.error("Fetch error:", error);
+    });
+}
